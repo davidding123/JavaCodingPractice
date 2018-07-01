bcf6c94bd5f2041fdd0b62c199a928159d31d6b7
davidding123 <davidding123@126.com>
2018-07-01 20:17:33 +0800

    Update strStr.java

--- StringOperations/strStr.java
+++ StringOperations/strStr.java
@@ -1,1 +1,6 @@
-
+class Solution{
+  public int strStr(String haystack, String needle){
+    if(haystack.length() == 0 && needle.length() == 0) return 0;
+    return haystack.indexOf(needle) >= 0? haystack.indexOf(needle):-1;
+  }
+}
