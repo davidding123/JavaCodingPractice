87eb400875424dcb1b0d846addc94d11fa673958
davidding123 <davidding123@126.com>
2018-07-01 19:41:55 +0800

    Update removeDuplicateFromSorted.java

--- ListOperations/removeDuplicateFromSorted.java
+++ ListOperations/removeDuplicateFromSorted.java
@@ -1,1 +1,11 @@
-
+class Solution{
+  public int removeDuplicats(int [] nums){
+    if(nums.length < 2) return nums.length;
+    int start = 1;
+    for(int i = 1; i < nums.length; i++){
+      if(nums[i] != nums[i-1]) nums[start++] = nums[i];
+    }
+    return start;
+  }
+}
+ 
