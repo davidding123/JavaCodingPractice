27ad35fbe289887147e3346cbc676053013e0713
davidding123 <davidding123@126.com>
2018-07-01 21:44:31 +0800

    Update insertionSortList.java

--- Sort/insertionSortList.java
+++ Sort/insertionSortList.java
@@ -1,1 +1,16 @@
-
+class Solution{
+  public ListNode insertionSortList(ListNode head){
+    if(head == null || head.next == null) return head;
+    ListNode helper = new ListNode(0);
+    ListNode cur = head, pre = helper, next = null;
+    while(cur != null){
+      next = cur.next;
+      while(pre.next != null && pre.next.val < cur.val) pre = pre.next;
+      cur.next = pre.next;
+      pre.next = cur;
+      pre = helper;
+      cur = next;
+    }
+    return helper.next;
+  }
+}
