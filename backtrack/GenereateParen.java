d9a12752f93cfa47855858814ff4f12aeb1e4b9e
davidding123 <davidding123@126.com>
2018-07-01 19:04:28 +0800

    Update GenereateParen.java

--- backtrack/GenereateParen.java
+++ backtrack/GenereateParen.java
@@ -1,1 +1,21 @@
-
+class Solution{
+  public List<String> generateParenthesis(int n){
+    List<String> list = new ArrayList<>();
+    if(n == 0) return list;
+    helper(list, "", n, 0, 0);
+    return list;
+  }
+  
+  private void helper(List<String> list, String cur, int n, int open, int close){
+    if(n == close){
+      list.add(cur);
+      return;
+    }
+    if(open < n){
+      helper(list, cur + "(", n, open+1, close);
+    }
+    if(close < open){
+      helper(list, cur + ")", n, open, close +1);
+    }
+  }
+}
