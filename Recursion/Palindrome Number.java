f979f099bcbd35bba85f96ab237a78dd02bc881d
davidding123 <davidding123@126.com>
2018-07-01 18:23:31 +0800

    Create Palindrome Number.java

---
+++ Recursion/Palindrome Number.java
@@ -0,0 +1,11 @@
+public class Solution{
+  public boolean IsP(int x){
+    if(x < 0 || (x!=0 && x%10 == 0)) return false;
+    int temp = 0;
+    while( x > temp){
+      temp = temp*10 + x%10;
+      x = x/10;
+      }
+      return x==temp || x == temp/10;
+      }
+     }
