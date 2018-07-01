c99d10193af7df404c604d5e680dcc1f545a0ec6
davidding123 <davidding123@126.com>
2018-07-01 18:51:47 +0800

    Update RemoveNthFromEnd.java

--- LinkedList/RemoveNthFromEnd.java
+++ LinkedList/RemoveNthFromEnd.java
@@ -1,1 +1,16 @@
+
 class Solution{
+  public ListNode removeNthFromEnd(ListNode head, int n){
+    ListNode start = new ListNode(0);
+    start.next = head;
+    ListNode slow = start, fast = start;
+    for(int i = 0; i <= n; i++){
+      fast = fast.next;
+    }
+    while(fast != null){
+      fast = fast.next;
+      slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return start.next;
+  }
