caa439c106f493cb3ef16256ba6f4bc136063370
davidding123 <davidding123@126.com>
2018-07-02 08:52:25 +0800

    Update bubbleSort.java

--- Sort/bubbleSort.java
+++ Sort/bubbleSort.java
@@ -1,1 +1,14 @@
-
+public void bubble(int [] r, int n){
+  int i = n - 1;
+  while(i > 0){
+    int pos = 0;
+    for(int j = 0; j < i; j++){
+      if(r[j] > r[j+1]）{
+        pos = j;
+        int tmp = r[j];
+        r[j] = r[j+1];
+        r[j+1] = tmp;
+      }
+         i = pos;
+         }
+         }
