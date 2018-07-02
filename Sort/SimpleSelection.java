b1ab92664b0bc7c2b8a0d8b715832b3da5bc3136
davidding123 <davidding123@126.com>
2018-07-02 08:25:56 +0800

    Update SimpleSelection.java

--- Sort/SimpleSelection.java
+++ Sort/SimpleSelection.java
@@ -1,1 +1,16 @@
+private void selectSort(int [] a){
+  for(int i = 0; i < a.length; i++){
+    int k = i;
+    for(int j = i+1; j < a.length; j++){
+      if(a[k] > a[j]) k = j;
+    }
+    swap(a, k, j);
+  }
+}
 
+private void swap(int [] a, int k, int j){
+  if(i == j) return;
+  int temp = a[k];
+  a[k] = a[j];
+  a[j] = temp;
+}
