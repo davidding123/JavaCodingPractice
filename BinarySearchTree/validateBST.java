7a131a1ed025b08b6e15d0734e680c9e95c7d67e
davidding123 <davidding123@126.com>
2018-07-02 14:33:25 +0800

    Update validateBST.java

--- BinarySearchTree/validateBST.java
+++ BinarySearchTree/validateBST.java
@@ -1,1 +1,10 @@
+public boolean isValid(TreeNode root){
+  if(root == null) return true;
+  return helper(root, Long.MAX_VALUE, Long.MIN_VALUE);
+}
 
+public void helper(TreeNode root, long max, long min){
+  if(root == null) return true;
+  if(root.val <= min || root.val >= max) return false;
+  return helper(root.left, root.val, min) && helper(root.right, max, root.val);
+}
