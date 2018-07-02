b21f1a734b5615ed52da514cb8d4c2e7af53f8fb
davidding123 <davidding123@126.com>
2018-07-02 08:14:39 +0800

    Update insertSort.java

--- Sort/insertSort.java
+++ Sort/insertSort.java
@@ -1,1 +1,9 @@
-
+public void insertSort(int [] a){
+  for(int i = 1; i < a.length; i++){
+    int j;
+    int x = a[i];
+    for(j = i; j > 0 && x < a[j-1]; j--){
+      a[j] = a[j-1];
+    }
+    a[j] = x;
+  }
