89f6a27c91e5a9df242688a261212765a8d20e22
davidding123 <davidding123@126.com>
2018-07-02 10:57:31 +0800

    Update findBottomLeft.java

--- BinarySearchTree/findBottomLeft.java
+++ BinarySearchTree/findBottomLeft.java
@@ -1,1 +1,11 @@
 //Given a binary tree, find the leftmost value in the last row of the tree.
+public int findBottomLeftValue(TreeNode root){
+  Queue<TreeNode> q = new LinkedList<>();
+  q.offer(root);
+  while(!q.isEmpty()){
+    root = q.poll();
+    if(root.right != null) q.offer(root.right);
+    if(root.left != null) q.offer(root.left);
+  }
+  return root.val;
+}
