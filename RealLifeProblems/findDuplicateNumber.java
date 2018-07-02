bded98548c9cb4cbe4ab4a27feacc21222a7a92e
davidding123 <davidding123@126.com>
2018-07-02 10:42:53 +0800

    Update findDuplicateNumber.java

--- RealLifeProblems/findDuplicateNumber.java
+++ RealLifeProblems/findDuplicateNumber.java
@@ -1,3 +1,19 @@
 //Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
 //prove that at least one duplicate number must exist. 
 //Assume that there is only one duplicate number, find the duplicate one.
+class Solution{
+  public int findDuplicate(int [] nums){
+    int n = nums.length;
+    int slow = n, fast = n;
+    do{
+      slow = nums[slow - 1];
+      fast = nums[nums[fast - 1] - 1];
+    }while(slow!=fast);
+    slow = n;
+    while(slow != fast){
+      slow = nums[slow - 1];
+      fast = nums[fast - 1];
+    }
+    return slow;
+  }
+}
