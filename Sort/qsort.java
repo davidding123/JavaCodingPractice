6d24af19263ac9105522a50ce5d41ef393c83a9f
davidding123 <davidding123@126.com>
2018-07-02 09:13:07 +0800

    Update qsort.java

--- Sort/qsort.java
+++ Sort/qsort.java
@@ -1,1 +1,22 @@
+private void quickSort(int [] a, int low, int high){
+  if(low < high) {
+    int middle = getMiddler(a, low, high);
+    quickSort(a, 0, middle - 1);
+    quickSort(a, middle + 1, high);
+  }
+}
 
+public int getMiddle(int [] a, int low, int high){
+  int key = a[low];
+  while(low < high){
+    while(low < high && a[high] >= key){
+      high--;
+      a[low] = a[high];
+      while(low < high && a[low] <= key){
+        low++;
+        a[high] = a[low];
+      }
+      a[low] = key;
+      return low;
+    }
+  }
