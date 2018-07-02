b8a01139c09dacda2fe91569b37bd799e8178bd3
davidding123 <davidding123@126.com>
2018-07-02 08:20:10 +0800

    Update shellSort.java

--- Sort/shellSort.java
+++ Sort/shellSort.java
@@ -1,1 +1,20 @@
+private void shellSort(int [] a){
+  int dk = a.length/2;
+  while(dk >= 1){
+    shellInsertSort(a, dk);
+    dk = dk/2;
+  }
+}
 
+private void shellInsertSort(int [] a, int dk){
+  for(int i = dk; i < a.length; i++){
+    if(a[i] < a[i-dk]){
+      int j;
+      int x = a[j];
+      for(j = i; j >= 0 && a[j] > x; j = j-dk){
+        a[j+dk] = a[j];
+      }
+      a[j+dk] = x;
+    }
+  }
+}
