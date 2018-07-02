0fde3ec6dc91cddd308608fea795ad8a10d37ed2
davidding123 <davidding123@126.com>
2018-07-02 14:40:31 +0800

    Update oddEven.java

--- LinkedList/oddEven.java
+++ LinkedList/oddEven.java
@@ -1,1 +1,13 @@
-
+public ListNode oddEvenList(ListNode head){
+  if(head == null || head.next == null) return head;
+  ListNode odd = head, evenHead = head.next;
+  ListNode even = evenHead;
+  while(even!=null && even.next !=null){
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+}
