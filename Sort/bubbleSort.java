de582ec978c2f9de9a36f7f6790ea62e18e6417e
davidding123 <davidding123@126.com>
2018-07-02 09:04:41 +0800

    Update bubbleSort.java

--- Sort/bubbleSort.java
+++ Sort/bubbleSort.java
@@ -4,3 +4,31 @@ public void bubbleSort(int [] a, int n){
       if(a[j] > a[j+1]) swap(a, j, j + 1);
     }
   }
+  
+  public void b_1(int [] r, int n){
+    int i = n - 1;
+    while(i > 0){
+      int pos = 0;
+      for(int j = 0; j < i; j++){
+        if(r[j] > r[j+1]){
+          pos = j;
+          swap(r, j, j +1);
+        }
+        i = pos;
+      }
+    }
+    
+    public void b_2(int [] r, int n){
+      int low = 0;
+      int hight = n - 1;
+      int tmp, j;
+      while(low < high){
+        for(j = low; j < high; ++j){
+          if(r[j] > r[j+1]) swap(r, j, j+1);
+          --high;
+        }
+        for(j = high; j > low; --j){
+          if(r[j] < r[j-1]) swapp(r, j, j-1);
+          ++low;
+        }
+      }
