8fae304c3965832b12b64b6637f2e2215e29dc4c
davidding123 <davidding123@126.com>
2018-07-21 16:55:26 +0800

    Update moveZero.cpp

--- CMRI/moveZero.cpp
+++ CMRI/moveZero.cpp
@@ -1,1 +1,9 @@
-
+int Func(int * a, int n){
+  int index = 0;
+  if(n <= 0) return index;
+  for(int i = 0; i < n; i++){
+    if(a[i] != 0) a[index++] = a[i];
+  }
+  for(int i = index; i < n; i++) a[i] = 0;
+  return index;
+}
