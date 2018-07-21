35a2ab4da0700321ac6ced8b3944273fae40d324
davidding123 <davidding123@126.com>
2018-07-21 17:18:14 +0800

    Update toUpper.cpp

--- CMRI/toUpper.cpp
+++ CMRI/toUpper.cpp
@@ -1,1 +1,13 @@
+#include "stdio.h"
+#include "conio.h"
 
+void uppers(char * s, char * us){
+  for(; *s != '\0'; s++, us++){
+    if(*s >= 'a' && *s <= 'z'){
+      *us = *s-32;
+    }else{
+      *us = *s;
+    }
+  }
+  *us = '\0';
+}
