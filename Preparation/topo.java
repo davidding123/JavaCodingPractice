f3d2baf9ad65695e59ad0e79116c1b366932d024
davidding123 <davidding123@126.com>
2018-09-11 17:18:07 +0800

    Update topo.java

--- Preparation/topo.java
+++ Preparation/topo.java
@@ -1,1 +1,13 @@
-
+void topsort () throws CycleFoundException{
+  Queue<Vertex> q = new Queue<Vertex>();
+  int counter = 0;
+  for each Vertex v 
+    if(v.indegree == 0) q.enqueue(v);
+  while(!q.isEmpty()){
+    Vertex v = q.dequeue();
+    v.topNum = ++counter;
+    for each Vertex w adjacent to v
+      if(--w.indegree == 0) q.enqueue(w);
+  }
+  if(counter != NUM_VERTICES) throw new CycleFoundException();
+}
