218bb38e091ddae2518f11adc9d9119edd0a5a0f
davidding123 <davidding123@126.com>
2018-09-11 19:46:13 +0800

    Update allPairs.java

--- Preparation/allPairs.java
+++ Preparation/allPairs.java
@@ -1,1 +1,18 @@
-
+public static void allPairs(int [][] a, int [][] d, int [][] path){
+  int n = a.length;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      d[i][j] = a[i][j];
+      path[i][j] = -1;
+    }
+  }
+  for(int k = 0; k < n; k++){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        if(d[i][k] + d[k][j] < d[i][j]){
+          d[i][j] = d[i][k] + d[k][j];
+          path[i][j] = k;
+        }
+      }
+    }
+  }
