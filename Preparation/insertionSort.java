3c200eab63f3fe89a0c829b7abe36bc6a976b780
davidding123 <davidding123@126.com>
2018-09-11 09:20:11 +0800

    Update insertionSort.java

--- Preparation/insertionSort.java
+++ Preparation/insertionSort.java
@@ -1,1 +1,12 @@
 public void insertionSort(int [] a){
+  int j;
+  for(int i = 1; i < a.length; i++){
+    int temp = a[i];
+    for(j = i; j > 0 && a[j] > a[j-1]; j--){
+      a[j] = a[j-1];
+    }
+    a[j] = temp;
+  }
+}
+  
+Time Complexitity: O(N^2)
