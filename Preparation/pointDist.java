366625659153d65d4004d33dae64e931f934ec92
davidding123 <davidding123@126.com>
2018-09-11 18:09:16 +0800

    Update pointDist.java

--- Preparation/pointDist.java
+++ Preparation/pointDist.java
@@ -1,1 +1,16 @@
+//brute force
+for(i = 0; i < numPointsInStrip; i++){
+  for(j = i + 1; j < numPointsInStrip; j++){
+    if(dist(pi, pj) < min) min = dist(pi, pj);
+  }
+}
 
+//Points are all in the strip and sorted by y-coordinate
+for(i = 0; i < numPointsInStrip; i++){
+  for(j = i + 1; j < numPointsInStrip; j++){
+    if(pi and pj's y-coordinate differ more than threshold) 
+       break;
+    else
+       min = Math.min(min, dist(pi, pj);
+                      }
+                      }
