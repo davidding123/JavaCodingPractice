7929b2ad360eae8ce4f761e58d69c562d9f94061
davidding123 <davidding123@126.com>
2018-09-11 09:26:40 +0800

    Update shellSort.java

--- Preparation/shellSort.java
+++ Preparation/shellSort.java
@@ -1,1 +1,16 @@
-
+public void shellSort(int [] a){
+  int j;
+  for(int gap = a.length/2; gap > 0; gap /= 2){
+    for(int i = gap; i < a.length; i++){
+      int temp = a[i];
+      for(int j = i; j >= gap && temp < a[j-gap]; j -= gap){
+        a[j] = a[j-gap];
+      }
+      a[j] = temp;
+    }
+  }
+  
+  
+  
+  time complexity: O(N^2) Hibbard: O(M^3/2)
+    
