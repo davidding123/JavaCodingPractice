1a9b8014404a0bdb2cc0d7c12b944e53b9429690
davidding123 <davidding123@126.com>
2018-09-11 17:28:47 +0800

    Update shortestPath.java

--- Preparation/shortestPath.java
+++ Preparation/shortestPath.java
@@ -1,1 +1,40 @@
-void unweigheted
+void unweigheted(Vertex s){
+  for each Vertex v{
+    v.dist = INFINITY;
+    v.known = false;
+  }
+  s.dist = 0;
+  for(int currDist = 0; currDist < NUM_VERTICES; currDist++){
+    for each Vertex v{
+      if(!v.known && v.dist == currDist){
+        v.known = true;
+        for each Vertex w adjacent to v{
+          if(w.dist == INFINITY){
+            w.dist = currDist + 1;
+            w.path = v;
+          }
+        }
+      }
+    }
+    
+    
+ void unweighted(Vertex s){
+   Queue<Vertex> q = new Queue<Vertex>();
+   for each Vertex v 
+     v.dist = INFINITY;
+   s.dist = 0;
+   q.enqueue(s);
+   while(!q.isEmpty()){
+     Vertex v = q.dequeue();
+     for each Vertex w adjacent to v
+       if(w.dist == INFINITY){
+         w.dist = v.dist + 1;
+         w.path = v;
+         q.enqueue(w);
+       }
+   }
+ }
+    
+    
+      
+ 
