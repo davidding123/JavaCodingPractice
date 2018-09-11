69fff129b0594aeb7b8e500762a463c098ecbb46
davidding123 <davidding123@126.com>
2018-09-11 18:12:38 +0800

    Update matrix.java

--- Preparation/matrix.java
+++ Preparation/matrix.java
@@ -1,1 +1,18 @@
-
+public static int [][] multiply(int [][] a, int [][] b){
+  int n = a.length;
+  int [][] c = new int[n][n];
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      c[i][j] = 0;
+    }
+  }
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      for(int k = 0; k < n; k++){
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+  return c;
+}
+      
