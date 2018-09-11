a3e3a320727f031af675e16bcc702d5eb2c0eeb7
davidding123 <davidding123@126.com>
2018-09-11 17:42:28 +0800

    Update wordLadder.java

--- Preparation/wordLadder.java
+++ Preparation/wordLadder.java
@@ -1,1 +1,28 @@
-
+public static List<String> findChain(Map<String, List<String>> adjacentWords, String first, String Second){
+  Map<String, String> previousWord = new HashMap<String, String>();
+  LinkedList<String> q = new LinkedList<String>();
+  q.addLast(first);
+  while(!q.isEmpty()){
+    String current = q.removeFirst();
+    List<String> adj = adjacentWords.get(current);
+    if(adj != null){
+      for(String adjWord : adj){
+        if(previousWord.get(adjWord) == null){
+          previousWord.put(adjWord, current);
+          q.addLast(adjWord);
+        }
+      }
+    }
+    previousWord.put(first, null);
+    return helper(previousWord, first, second);
+  }
+  
+  public static List<String> helper(Map<String, String> prev, String first, String second){
+    LinkedList<String> result = null;
+    if(prev.get(second) != null){
+      result = new LinkedList<String>();
+      for(String str = second; str != null; str = prev.get(str)){
+        result.addFirst(str);
+      }
+      return result;
+    }
