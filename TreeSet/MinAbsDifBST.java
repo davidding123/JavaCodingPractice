47f1ef46dca899c50e418734ee87289826b5b16e
davidding123 <davidding123@126.com>
2019-03-08 10:49:24 +0800

    Update MinAbsDifBST.java

--- TreeSet/MinAbsDifBST.java
+++ TreeSet/MinAbsDifBST.java
@@ -1,1 +1,14 @@
-
+public class Solution{
+  int min = Integer.MAX_VALUE;
+  Integer prev = null;
+  public int getMinDiff(TreeNode root){
+    if(root == null) return min;
+    getMinDiff(root.left);
+    if(prev != null){
+      min = Math.min(min, root.val - prev);
+    }
+    prev = root.val;
+    getMinDiff(root.right);
+    return min;
+  }
+}
