fc762881b3d7826ec87832d909507057e01e5f93
davidding123 <davidding123@126.com>
2019-03-08 11:03:32 +0800

    Update sumOfLeftLeaves.java

--- BinarySearchTree/sumOfLeftLeaves.java
+++ BinarySearchTree/sumOfLeftLeaves.java
@@ -1,1 +1,20 @@
-
+class Solution{
+  public int sumOfLeftLeaves(TreeNode root){
+    if(root == null) return 0;
+    int sum = 0;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.push(root);
+    while(!stack.isEmpty()){
+      TreeNode p = stack.pop();
+      if(p.left != null){
+        if(p.left.left == null && p.left.right == null){
+          sum += p.left.val;
+        }else{
+          stack.push(p.left);
+        }
+      }
+      if(p.right != null) stack.push(p.right);
+    }
+    return sum;
+  }
+}
