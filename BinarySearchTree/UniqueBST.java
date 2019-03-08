f35f4674e9dc3dc4c9d1510e23ba386c0d1317df
davidding123 <davidding123@126.com>
2019-03-08 10:54:43 +0800

    Update UniqueBST.java

--- BinarySearchTree/UniqueBST.java
+++ BinarySearchTree/UniqueBST.java
@@ -1,1 +1,28 @@
-
+class Solution{
+  public List<TreeNode> generateTrees(int n){
+    List<TreeNode> list = new ArrayList<>();
+    if(n <= 0) return list;
+    return helper(1, n);
+  }
+  
+  private List<TreeNode> helper(int l, int r){
+    List<TreeNode> list = new ArrayList<>();
+    if(l > r){
+      list.add(null);
+      return list;
+    }
+    for(int i = l; i <= r; i++){
+      List<TreeNode> leftSub = helper(l, i - 1);
+      List<TreeNode> rightSub = helper(i + 1, r);
+      for(TreeNode left : leftSub){
+        for(TreeNode right : rightSub){
+          TreeNode root = new TreeNode(i);
+          root.left = left;
+          root.right = right;
+          list.add(root);
+        }
+      }
+    }
+    return list;
+  }
+}
