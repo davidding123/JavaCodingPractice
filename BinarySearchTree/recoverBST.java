30a23e02485fa2ea131848ba5e3ce2419656bfd7
davidding123 <davidding123@126.com>
2019-03-08 10:58:55 +0800

    Update recoverBST.java

--- BinarySearchTree/recoverBST.java
+++ BinarySearchTree/recoverBST.java
@@ -1,1 +1,24 @@
-
+class Solution{
+  TreeNode first = null;
+  TreeNode second = null;
+  TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+  public void recoverTree(TreeNode root){
+    traverse(root);
+    int temp = first.val;
+    first.val = second.val;
+    second.val = temp;
+  }
+  
+  private void traverse(TreeNode root){
+    if(root == null) return;
+    traverse(root.left);
+    if(first == null && prev.val >= root.val){
+      first = prev;
+    }
+    if(first != null && prev.val >= root.val){
+      second = root;
+    }
+    prev = root;
+    traverse(root.right);
+  }
+}
