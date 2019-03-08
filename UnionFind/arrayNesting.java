2f8b1dc44c5091c0219eb1c70a505a7d9a19f829
davidding123 <davidding123@126.com>
2019-03-08 10:46:01 +0800

    Update arrayNesting.java

--- UnionFind/arrayNesting.java
+++ UnionFind/arrayNesting.java
@@ -1,1 +1,22 @@
-
+class Solution{
+  public int arrayNesting(int [] nums){
+    int max = Integer.MIN_VALUE;
+    boolean [] isVisited = new boolean[nums.length];
+    for(int i = 0; i < nums.length; i++){
+      if(isVisited[i]) continue;
+      max = Math.max(max, helper(nums, i, isVisited));
+    }
+    return max;
+  }
+  
+  public int helper(int [] nums, int start, boolean [] isVisited){
+    int i = start, count = 0;
+    while(count == 0 || i != start){
+      isVisited[i] = true;
+      i = nums[i];
+      count++;
+    }
+    return count;
+  }
+}
+                    
