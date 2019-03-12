c063011a5a71fac0adf7612ff8200d3c9620cebc
davidding123 <davidding123@126.com>
2019-03-12 09:21:50 +0800

    Update reverseWords.java

--- StringOperations/reverseWords.java
+++ StringOperations/reverseWords.java
@@ -1,1 +1,23 @@
-
+public class Solution{
+  public String reverseWords(String s){
+    char [] ca = s.toCharArray();
+    for(int i = 0; i < ca.length; i++){
+      if(ca[i] != ' '){
+        int j = i;
+        while(j + 1 < ca.length && ca[j + 1] != ' ') j++;
+        reverse(ca, i, j);
+        i = j;
+      }
+    }
+    return new String(ca);
+  }
+  private void reverse(char [] s, int i, int j){
+    while(i < j){
+      char temp s[i];
+      s[i] = s[j];
+      s[j] = temp;
+      i++;
+      j--;
+    }
+  }
+}
