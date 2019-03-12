be6a48b0149ab69b9432f966e8145bf63e2c3026
davidding123 <davidding123@126.com>
2019-03-12 09:07:14 +0800

    Update convertBST.java

--- BinarySearchTree/convertBST.java
+++ BinarySearchTree/convertBST.java
@@ -1,1 +1,15 @@
-
+class Solution{
+  int sum = 0;
+  public TreeNode convertBST(TreeNode root){
+    if(root == null) return null;
+    helper(root);
+    return root;
+  }
+  private void helper(TreeNode root){
+    if(root == null) return;
+    helper(root.right);
+    root.val += sum;
+    sum = root.val;
+    helper(root.left);
+  }
+}
