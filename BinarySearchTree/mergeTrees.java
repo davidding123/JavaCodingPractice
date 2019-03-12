86e2f7d29ade1f9a721caef49286c83cacb065a5
davidding123 <davidding123@126.com>
2019-03-12 09:12:00 +0800

    Update mergeTrees.java

--- BinarySearchTree/mergeTrees.java
+++ BinarySearchTree/mergeTrees.java
@@ -1,1 +1,10 @@
-
+public class Solution{
+  public TreeNode mergeTrees(TreeNode t1, TreeNode t2){
+    if(t1 == null && t2 ==  null) return null;
+    int val = (t1 == null ? 0:t1.val) + (t2 == null ? 0: t2.val);
+    TreeNode node = new TreeNode(val);
+    node.left = mergeTrees(t1 == null?null:t1.left, t2 == null?null: t2.left);
+    node.right = mergeTrees(t1 == null? null:t1.right, t2 == null?null: t2.right);
+    return node;
+  }
+}
