c4ca38c97e552bcc2112e8ae029a593a2d56e8ce
davidding123 <davidding123@126.com>
2019-03-12 09:03:55 +0800

    Update BTmaxSum.java

--- BinarySearchTree/BTmaxSum.java
+++ BinarySearchTree/BTmaxSum.java
@@ -1,1 +1,14 @@
-
+class Solution{
+  int max = Integer.MIN_VALUE;
+  public int maxPathSum(TreeNode root){
+    if(root == null) return 0;
+    helper(root);
+    return max;
+  }
+  public int helper(TreeNode root){
+    if(root == null) return  0;
+    int left = helper(root.left);
+    int right = helper(root.right);
+    max = Math.max(Math.max(left, 0) + Math.max(right, 0) + root.val, max);
+  }
+}
