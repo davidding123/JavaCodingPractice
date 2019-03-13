52a7393a734f7ac97131960dfd6d9c853ad2b77d
davidding123 <davidding123@126.com>
2019-03-13 09:25:07 +0800

    Update connectTreeLink.java

--- BinarySearchTree/connectTreeLink.java
+++ BinarySearchTree/connectTreeLink.java
@@ -10,4 +10,25 @@ public class Solution{
     connect(root.left);
     connect(root.right);
   }
+  
+  public void connect2(TreeLinkNode root){
+    TreeLinkNode dummyHead = new TreeLinkNode(0);
+    TreeLinkNode pre = dummyHead;
+    while(root != null){
+      if(root.left != null){
+        pre.next = root.left;
+        pre = pre.next;
+      }
+      if(root.right != null){
+        pre.next = root.right;
+        pre = pre.next;
+      }
+      root = root.next;
+      if(root == null){
+        pre = dummyHead;
+        root = dummyHead.next;
+        dummyHead.next = null;
+      }
+    }
+  }
 }
