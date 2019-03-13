ad2e27fbf719fd37af342a5fabdb31ae6c6c4c31
davidding123 <davidding123@126.com>
2019-03-13 09:11:23 +0800

    Update countSubstrings.java

--- StringOperations/countSubstrings.java
+++ StringOperations/countSubstrings.java
@@ -1,1 +1,18 @@
-
+public class Solution{
+  int count = 0;
+  public int countSubstrings(String s){
+    if(s == null || s.length() == 0) return 0;
+    for(int i = 0; i < s.length(); i++){
+      helper(s, i, i);
+      helper(s, i, i + 1);
+    }
+    return count;
+  }
+  private void helper(String s, int left, int right){
+    while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+      count++;
+      left--;
+      right++;
+    }
+  }
+}
