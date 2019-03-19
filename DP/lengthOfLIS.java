f50578559c0bf995506cd92981994ea3f7a921f3
davidding123 <davidding123@126.com>
2019-03-19 09:06:40 +0800

    Update lengthOfLIS.java

--- DP/lengthOfLIS.java
+++ DP/lengthOfLIS.java
@@ -1,1 +1,16 @@
-
+class Solution{
+  public int lengthOfLIS(int [] nums){
+    if(nums == null || nums.length == 0) return 0;
+    if(nums.length == 1) return 1;
+    int [] dp = new int[nums.length + 1];
+    Arrays.fill(dp, 1);
+    for(int i = 1; i < nums.length; i++){
+      for(int j = 0; j < i; j++){
+        if(nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    int max = 1;
+    for(int i : dp) max = Math.max(max, i);
+    return max;
+  }
+}
