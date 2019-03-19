e166d88b8ff4395ac79432e656743f7bd74d29ed
davidding123 <davidding123@126.com>
2019-03-19 09:01:24 +0800

    Update perfectSquares.java

--- DP/perfectSquares.java
+++ DP/perfectSquares.java
@@ -1,1 +1,15 @@
-
+class Solution{
+  public int numSquares(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    int [] dp = new int[n + 1];
+    Arrays.fill(dp, n);
+    dp[0] = 0;
+    for(int i = 1; i <= n; i++){
+      for(int j = 1; j*j <= i; j++){
+        dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+      }
+    }
+    return dp[n];
+  }
+}
