501c0866b5e25642ebbbc922d9992c7e12c3ec82
davidding123 <davidding123@126.com>
2019-03-20 09:27:24 +0800

    Update coinChange.java

--- DP/coinChange.java
+++ DP/coinChange.java
@@ -1,1 +1,16 @@
-
+class Solution{
+  public int coinChange(int [] coins, int amount){
+    if(coins == null || coins.length == 0 || amount < 0) return -1;
+    if(amount == 0) return 0;
+    Arrays.sort(coins);
+    int [] dp = new int[amount + 1];
+    Arrays.fill(dp, amount + 1);
+    dp[0] = 0;
+    for(int i = 1; i <= amount; i++){
+      for(int j = 0; j < coins.length; j++){
+        if(coins[j] <= i) dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+    return dp[amount] == amount + 1?-1:dp[amount];
+  }
+}
