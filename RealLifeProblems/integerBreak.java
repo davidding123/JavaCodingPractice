b2b8eb37fcdfa718bfd0bd5cd2cb0fd35a32056f
davidding123 <davidding123@126.com>
2019-03-20 09:35:05 +0800

    Update integerBreak.java

--- RealLifeProblems/integerBreak.java
+++ RealLifeProblems/integerBreak.java
@@ -1,1 +1,13 @@
-
+class Solution{
+  public int integerBreak(int n){
+    if(n == 2) return 1;
+    if(n == 3) return 2;
+    if(n == 4) return 4;
+    int ans = 1;
+    while(n > 4){
+      ans *= 3;
+      n -= 3;
+    }
+    return n*ans;
+  }
+}
