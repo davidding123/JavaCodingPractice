61d8d59afe1eea6ba271de2e083e88ba61d93a5c
davidding123 <davidding123@126.com>
2019-03-22 09:25:22 +0800

    Update partitionEqual.java

--- DP/partitionEqual.java
+++ DP/partitionEqual.java
@@ -1,1 +1,18 @@
-
+class Solution{
+  public boolean canPartition(int [] nums){
+    if(nums == null || nums.length == 0) return false;
+    Arrays.sort(nums);
+    int sum = 0;
+    for(int num : nums) sum += num;
+    if((sum&1) != 0) return false;
+    sum = sum >> 1;
+    boolean [] dp = new boolean[sum + 1];
+    dp[0] = true;
+    for(int num : nums){
+      for(int i = sum; i >= 1; i--){
+        if(num <= i) dp[i] = dp[i] || dp[i - num];
+      }
+    }
+    return dp[sum];
+  }
+}
