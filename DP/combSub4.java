1ada9f38b95a6494970cd1e72de4dc1e9ee574c9
davidding123 <davidding123@126.com>
2019-03-22 09:20:02 +0800

    Update combSub4.java

--- DP/combSub4.java
+++ DP/combSub4.java
@@ -1,1 +1,14 @@
-
+class Solution{
+  public int combinationSum4(int [] nums, int target){
+    if(nums == null || nums.length == 0 || target < 0) return 0;
+    int [] dp = new int[target + 1];
+    Arrays.sort(nums);
+    dp[0] = 1;
+    for(int i = 1; i <= target; i++){
+      for(int num : nums){
+        if(num <= i) dp[i] = dp[i] + dp[i - num];
+      }
+    }
+    return dp[target];
+  }
+}
