2cac206a16b565bb50d39ca63475b2bc1517d945
davidding123 <davidding123@126.com>
2019-06-30 15:10:37 +0800

    Create shortestDijkstra.java

---
+++ classicAlgorithms/graph/shortestDijkstra.java
@@ -0,0 +1,49 @@
+    // Funtion that implements Dijkstra's single source shortest path 
+    // algorithm for a graph represented using adjacency matrix 
+    // representation 
+    void dijkstra(int graph[][], int src) 
+    { 
+        int dist[] = new int[V]; // The output array. dist[i] will hold 
+                                 // the shortest distance from src to i 
+  
+        // sptSet[i] will true if vertex i is included in shortest 
+        // path tree or shortest distance from src to i is finalized 
+        Boolean sptSet[] = new Boolean[V]; 
+  
+        // Initialize all distances as INFINITE and stpSet[] as false 
+        for (int i = 0; i < V; i++) 
+        { 
+            dist[i] = Integer.MAX_VALUE; 
+            sptSet[i] = false; 
+        } 
+  
+        // Distance of source vertex from itself is always 0 
+        dist[src] = 0; 
+  
+        // Find shortest path for all vertices 
+        for (int count = 0; count < V-1; count++) 
+        { 
+            // Pick the minimum distance vertex from the set of vertices 
+            // not yet processed. u is always equal to src in first 
+            // iteration. 
+            int u = minDistance(dist, sptSet); 
+  
+            // Mark the picked vertex as processed 
+            sptSet[u] = true; 
+  
+            // Update dist value of the adjacent vertices of the 
+            // picked vertex. 
+            for (int v = 0; v < V; v++) 
+  
+                // Update dist[v] only if is not in sptSet, there is an 
+                // edge from u to v, and total weight of path from src to 
+                // v through u is smaller than current value of dist[v] 
+                if (!sptSet[v] && graph[u][v]!=0 && 
+                        dist[u] != Integer.MAX_VALUE && 
+                        dist[u]+graph[u][v] < dist[v]) 
+                    dist[v] = dist[u] + graph[u][v]; 
+        } 
+  
+        // print the constructed distance array 
+        printSolution(dist, V); 
+    } 
