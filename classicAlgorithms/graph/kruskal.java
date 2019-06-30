1819b2e88da0bb66e49dab44e850068bac54cd65
davidding123 <davidding123@126.com>
2019-06-30 15:19:45 +0800

    Create kruskal.java

---
+++ classicAlgorithms/graph/kruskal.java
@@ -0,0 +1,58 @@
+    void KruskalMST() 
+    { 
+        Edge result[] = new Edge[V];  // Tnis will store the resultant MST 
+        int e = 0;  // An index variable, used for result[] 
+        int i = 0;  // An index variable, used for sorted edges 
+        for (i=0; i<V; ++i) 
+            result[i] = new Edge(); 
+  
+        // Step 1:  Sort all the edges in non-decreasing order of their 
+        // weight.  If we are not allowed to change the given graph, we 
+        // can create a copy of array of edges 
+        Arrays.sort(edge); 
+  
+        // Allocate memory for creating V ssubsets 
+        subset subsets[] = new subset[V]; 
+        for(i=0; i<V; ++i) 
+            subsets[i]=new subset(); 
+  
+        // Create V subsets with single elements 
+        for (int v = 0; v < V; ++v) 
+        { 
+            subsets[v].parent = v; 
+            subsets[v].rank = 0; 
+        } 
+  
+        i = 0;  // Index used to pick next edge 
+  
+        // Number of edges to be taken is equal to V-1 
+        while (e < V - 1) 
+        { 
+            // Step 2: Pick the smallest edge. And increment  
+            // the index for next iteration 
+            Edge next_edge = new Edge(); 
+            next_edge = edge[i++]; 
+  
+            int x = find(subsets, next_edge.src); 
+            int y = find(subsets, next_edge.dest); 
+  
+            // If including this edge does't cause cycle, 
+            // include it in result and increment the index  
+            // of result for next edge 
+            if (x != y) 
+            { 
+                result[e++] = next_edge; 
+                Union(subsets, x, y); 
+            } 
+            // Else discard the next_edge 
+        } 
+  
+        // print the contents of result[] to display 
+        // the built MST 
+        System.out.println("Following are the edges in " +  
+                                     "the constructed MST"); 
+        for (i = 0; i < e; ++i) 
+            System.out.println(result[i].src+" -- " +  
+                   result[i].dest+" == " + result[i].weight); 
+    } 
+  
