5d460add34bf598c422da90dc4dcc5ffb3f82e18
davidding123 <davidding123@126.com>
2019-07-08 09:49:15 +0800

    Create numberOfBitsFlippedFromAtoB.java

---
+++ classicAlgorithms/bitManipulation/numberOfBitsFlippedFromAtoB.java
@@ -0,0 +1,34 @@
+// Count number of bits to be flipped 
+// to convert A into B 
+import java.util.*; 
+  
+class Count { 
+      
+    // Function that count set bits 
+    public static int countSetBits(int n) 
+    { 
+        int count = 0; 
+        while (n != 0) { 
+            count += n & 1; 
+            n >>= 1; 
+        } 
+        return count; 
+    } 
+  
+    // Function that return count of 
+    // flipped number 
+    public static int FlippedCount(int a, int b) 
+    { 
+        // Return count of set bits in 
+        // a XOR b 
+        return countSetBits(a ^ b); 
+    } 
+      
+    // Driver code 
+    public static void main(String[] args) 
+    { 
+        int a = 10; 
+        int b = 20; 
+        System.out.print(FlippedCount(a, b)); 
+    } 
+} 
